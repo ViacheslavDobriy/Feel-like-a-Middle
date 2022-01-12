b85000ea3228e5a8b6c395a011e1f3b3d537d3e6
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-12 13:26:44 +0300

    Добавил решение, которое выводит таблицу квадратов от 1 до N

---
+++ 1_Task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число N: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int UserN = GetNumber();
+for(int i =1; i<=UserN;i++)
+{
+    Console.WriteLine($"{i} * {i} = {i*i}");
+}
