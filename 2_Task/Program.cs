28f24cf1616e130f2ae21bb728d883092968be99
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-12 13:33:32 +0300

    Программа выводит значение кубов чисел от 1 до N

---
+++ 2_Task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число N: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int UserN = GetNumber();
+for(int j=1; j<=UserN;j++)
+{
+    Console.WriteLine($" {j}^3 = {j*j*j}");
+}
