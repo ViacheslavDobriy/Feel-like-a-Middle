386c2dbc6e183854f71948845a5e3aabc98cdfa6
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-12 13:43:48 +0300

    Результатом программы является сумма всех чисел от 1 до A

---
+++ 3_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму чисел от 1 до А
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число A: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int ResultSum(int NumberA)
+{
+    int result = 0;
+    for(int k=1;k<=NumberA;k++)
+    {
+        result=result+k;
+    }
+    return result;
+}
+int UserA = GetNumber();
+Console.WriteLine($"Сумма всех чисел от 1 до A = {ResultSum(UserA)}");
