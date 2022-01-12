1e7c2f0ce95c3cf912e4adc20751181b953650b6
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-12 13:57:06 +0300

    Программа выдает результат возведения числа A в степень B через использование цикла for

---
+++ 4_Task/Program.cs
@@ -0,0 +1,26 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int GetNumberA()
+{
+    Console.WriteLine("Введите число A: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int GetNumberB()
+{
+    Console.WriteLine("Введите степень числа A: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int GiveResult(int number, int Pow)
+{
+    int Result = 1;
+    for(int i = 1; i<=Pow; i++)
+    {
+        Result=Result*number;
+    }
+    return Result;
+}
+int UserA = GetNumberA();
+int UserB = GetNumberB();
+Console.WriteLine($"Результатом возведения числа {UserA} в {UserB} степень является - {GiveResult(UserA,UserB)}");
