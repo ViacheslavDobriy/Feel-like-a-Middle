eeef64f6aadee1e47b0649ba6e4d88ed7d402372
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-12 14:09:30 +0300

    Решение задачи выдает на экран количество цифр в введенном числе

---
+++ 5_Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Определить количество цифр в числе
+
+double GetNumber()
+{
+    Console.WriteLine("Введите число : ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double UserN = GetNumber();
+int i =1;
+for(i=1; Math.Round(UserN/10-0,5)>=1;i++)
+{
+    UserN=Math.Round(UserN/10-0,5);
+}
+Console.WriteLine($"Всего в введенном числе {i} цифр");
