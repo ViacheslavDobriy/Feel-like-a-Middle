ed546632bb4e577c28429dde4787d8f70fc259ca
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-14 16:02:25 +0300

    Программа выдает на экран кубы всех четных чисел на отрезке от 1 до числа введенного с клавиатуры

---
+++ 8_Task/Program.cs
@@ -0,0 +1,20 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число, до которого нужно показать кубы четных чисел: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+void ShowCub(int number)
+{
+    for(int i = 2; i <=number; i++)
+    {
+        if(i%2==0)
+        {
+            Console.WriteLine($"{i}^3 = {i*i*i} ");
+        }
+    }
+}
+int UserN = GetNumber();
+ShowCub(UserN);
