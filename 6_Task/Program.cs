8df6be18ac7340cef78ceb63ddb69f7fb446c699
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-14 15:35:43 +0300

    Упростил код, поменял все типы данных с double на int. Программа работает

---
+++ 6_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿// Подсчитать сумму цифр в числе
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int UserNumber = GetNumber();
+int result = 0;
+while(UserNumber>=1)
+{
+    result = result + UserNumber%10;
+    UserNumber=UserNumber/10;
+}
+Console.WriteLine($"Сумма цифр в введенном числе равна {result}");
