f9425d24493545cdb8af85c94e133a4cd954a9e5
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-14 15:50:24 +0300

    В результате программа выводит произведение всех чисел от 1 до введенного числа с клавиатуры

---
+++ 7_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число N: ");
+    int number=Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int Multiple(int number)
+{
+    int result = 1;
+    for(int i = 1 ; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+int UserNumber = GetNumber();
+Console.WriteLine($"Произведение всех чисел от 1 до {UserNumber} = {Multiple(UserNumber)}");
